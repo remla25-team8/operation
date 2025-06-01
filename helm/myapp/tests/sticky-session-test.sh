8961429d389e424511637a732b719b55d279ecf0
Todor Mladenovic <todor.malto.mladenovic@gmail.com>
2025-06-01 19:14:33 +0200

    updated the test-script

--- helm/myapp/tests/sticky-session-test.sh
+++ helm/myapp/tests/sticky-session-test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
      echo "Testing default routing (90% v1, 10% v2)"
      for i in {1..10}; do
-       curl -H "Host: app.local" http://192.168.56.91/predict -d '{"text": "Great food!"}' -H "Content-Type: application/json"
+       curl -H "Host: app.local" http://192.168.56.91/analyze -d '{"review": "Great food!"}' -H "Content-Type: application/json"
      done
 
      echo "Testing Sticky Session with x-user-id=test-user (always v2)"
      for i in {1..5}; do
-       curl -H "Host: app.local" -H "x-user-id: test-user" http://192.168.56.91/predict -d '{"text": "Great food!"}' -H "Content-Type: application/json"
+       curl -H "Host: app.local" -H "x-user-id: test-user" http://192.168.56.91/analyze -d '{"review": "Great food!"}' -H "Content-Type: application/json"
      done
